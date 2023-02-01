9fe5033e72bf39ee05c52ae84c8fb9491244194a
celularfreak <celularfreak@hotmail.com>
2023-02-01 21:52:01 +0100

    un poco mas

---
+++ Prueba-web/script.js
@@ -0,0 +1,42 @@
+/*var maria = document.getElementsByClassName("maria");
+var antonio = document.getElementsByClassName("antonio");
+var style1 = document.getElementsByClassName("style1");
+var style2 = document.getElementsByClassName("style2");
+
+maria[0].addEventListener("click", changebutton);
+antonio[0].addEventListener("click", changebutton);
+style1[0].addEventListener("click", changebutton2);
+style2[0].addEventListener("click", changebutton2);
+
+function changebutton	() {
+	if (maria[0].style.backgroundColor == "rgb(45, 120, 149)") {
+		maria[0].style.backgroundColor = "#f1f1f1";
+		maria[0].style.color = "#2d7895";
+		antonio[0].style.backgroundColor = "#2d7895";
+		antonio[0].style.color = "#f1f1f1";
+		
+	} else if (maria[0].style.backgroundColor == "rgb(241, 241, 241)"){
+		maria[0].style.backgroundColor = "#2d7895";
+		maria[0].style.color = "#f1f1f1";
+		antonio[0].style.backgroundColor = "#f1f1f1";
+		antonio[0].style.color = "#2d7895";
+	}
+}
+
+function changebutton2	() {
+	if (style1[0].style.backgroundColor == "rgb(45, 120, 149)") {
+		style1[0].style.backgroundColor = "#f1f1f1";
+		style1[0].style.color = "#2d7895";
+		style2[0].style.backgroundColor = "#2d7895";
+		style2[0].style.color = "#f1f1f1";
+		
+	} else {
+		style1[0].style.backgroundColor = "#2d7895";
+		style1[0].style.color = "#f1f1f1";
+		style2[0].style.backgroundColor = "#f1f1f1";
+		style2[0].style.color = "#2d7895";
+		
+	}
+}
+*/
+
